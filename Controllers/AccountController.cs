a16afbbf62c6e826b47d9f783a199dc466d1c2b4
regismael <ismael.l.regis@gmail.com>
2023-07-04 18:10:45 -0300

    first commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContatosAppPresentation.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult PasswordRecover()
+        {
+            return View();
+        }
+    }
+}
